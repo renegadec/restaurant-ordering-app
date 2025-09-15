312fb16e0ab558714361265828424f8d16a649ae
Renegade Confidence <confidencenyirenda@Renegades-MacBook-Air.local>
2025-09-15 17:53:29 +0200

    added functionality to add food to cart

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import { menuArray as menu } from './data/data'
 
-const main = document.getElementById('main')
+const foodMenu = document.getElementById('food-menu')
+const checkoutEl = document.getElementById('checkout')
+const checkoutItems = document.getElementById('checked-out')
+const totalPrice =document.getElementById('total')
+
+document.addEventListener('click', function(e){
+    if(e.target.dataset.add){
+        handleCheckout(e.target.dataset.add);
+        checkoutEl.style.display = 'block'
+    }
+})
 
 const menuItems = menu.map(function(menuItem){
     return `
@@ -11,15 +21,46 @@ const menuItems = menu.map(function(menuItem){
             <p class="ingredients">${menuItem.ingredients}</p>
             <p class="price"> $${menuItem.price}</p>
         </div>
-        <div class="add-btn">+</div>
-     </div>  
+        <div class="add-btn" data-add="${menuItem.id}">+</div>
+     </div>
     `
 })
-console.log(menuItems)
+
+const cartItems = []
+
+function handleCheckout (foodId) {
+    
+    const checkedOut = menu.filter(function(menuItem){
+        const id = Number(foodId)
+        return menuItem.id === id
+    })[0]
+
+    if (!checkedOut) return;
+
+    cartItems.push(checkedOut)
+
+    const cartTotalPrice = cartItems.reduce(function(sum, item){
+        return sum + item.price
+    }, 0)
+
+    checkoutItems.innerHTML += `
+        <div>
+            <p>${checkedOut.name}<span class="remove-btn">remove</span><p> 
+            <p>$${checkedOut.price}</p>
+        </div>
+    `
+
+    totalPrice.innerHTML = `
+            <p>Total Price:</p>
+            <p>${cartTotalPrice}</p>
+    `
+}
+
+
 
 
 function renderFoodItem() {
-     main.innerHTML = menuItems
+     foodMenu.innerHTML = menuItems
 }
 
 renderFoodItem()
